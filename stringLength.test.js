2c53e49ea1897247384547c39c66925a9b7a3ad3
ibizugbe <abrahamibizugbe@gmail.com>
2022-07-13 11:05:30 +0100

    test stringLength function

---
+++ stringLength.test.js
@@ -0,0 +1,4 @@
+const stringLength = require("./stringLength");
+test("gets the length of the string 'john' and returns 4 as the length", () => {
+  expect(stringLength("Johniwhwihwihw")).toBe("fail");
+});
